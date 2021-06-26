f82ca1f832e517aa84ee3ed35a2be3883d620119
imrishuroy <rishukumar.prince@gmail.com>
2021-06-26 23:48:40 +0530

    Initial Project Setup

---
+++ android/app/src/main/kotlin/com/example/portfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sixteenbrains.portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Welcome to Rishu Portfolio',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
